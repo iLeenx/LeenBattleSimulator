0682335f271ce38ed4045222097332a0a5664909
iLeenx <LeenALshehrii@hotmail.com>
2026-02-15 09:27:44 +0300

    deleted spawners scripts

--- Assets/Authoring/SpawnerData.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Entities;
-
-public struct SpawnerData : IComponentData
-{
-    public Entity UnitPrefab;
-    public int UnitsPerTeam;
-    public float Spacing;
-    public float TeamOffset;
-}
